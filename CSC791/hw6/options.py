59dc6d4781a0dd578737527b85d20a69950f2071
Vivek Nair <vivekaxl@gmail.com>
2014-10-14 08:33:07 -0400

    added hw6

---
+++ CSC791/hw6/options.py
@@ -0,0 +1,8 @@
+from __future__ import division 
+import sys
+sys.dont_write_bytecode = True
+
+myoptions = {'MaxWalkSat':{'maxTries':'50','maxChanges':'2000','threshold':'0.001','probLocalSearch':'0.25'},'SA':{'kmax':'1000','emax':'0'},'GA':{'crossOverRate':'0.6','mutationRate':'0.1','elitism':'50','generation':'20'},'DE':{'repeat':100,'np':100,'f':0.75,'cf':0.3}}
+
+myModeloptions = {'Lives': 4,'a12':0.56}
+myModelobjf = {'Viennet':3,'Schaffer':2, 'Fonseca':2, 'Kursawe':2, 'ZDT1':2,'ZDT3':2,'DTLZ7':20,'Schwefel':1}
